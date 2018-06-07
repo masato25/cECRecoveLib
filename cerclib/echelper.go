8a271ab17a62ff497f4ed4560c42c76625c4b5ec
masato25 <leomasato25@gmail.com>
2018-06-07 18:13:48 +0800

    fix version 1

---
+++ cerclib/echelper.go
@@ -0,0 +1,40 @@
+package echelper
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/signer/core"
+	"strings"
+)
+
+type EcRevoerFeilds struct {
+	EthAddress        string `form:"eth_address" json:"eth_address" binding:"required"`
+	EthereumSignature string `from:"eth_signature" json:"eth_signature" binding:"required"`
+	Msg               string `form:"msg" json:"msg"`
+}
+
+func signHash(data []byte) []byte {
+	msg := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), data)
+	return crypto.Keccak256([]byte(msg))
+}
+
+func decodeEthHash(inputs EcRevoerFeilds) (recoveredAddrHex string, err error) {
+	sig := hexutil.MustDecode(inputs.EthereumSignature)
+	sigMg := []byte(inputs.Msg)
+	personal := core.SignerAPI{}
+	recoveredAddr, err := personal.EcRecover(nil, sigMg, sig)
+	if err == nil {
+		recoveredAddrHex = fmt.Sprintf("0x%x", recoveredAddr)
+		recoveredAddrHex = strings.ToLower(recoveredAddrHex)
+	}
+	return
+}
+
+func Resolve(inputs EcRevoerFeilds) (*string, error) {
+	recoveredAddrHex, err := decodeEthHash(inputs)
+	if err != nil {
+		return nil, err
+	}
+	return &recoveredAddrHex, err
+}
---
+++ cerclib/echelper_test.go
@@ -0,0 +1,31 @@
+package echelper
+
+import (
+	_ "github.com/sirupsen/logrus"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestSpace(t *testing.T) {
+	Convey("Test 1 == 1", t, func() {
+		x := 1
+		Convey("When x incremented", func() {
+			x++
+
+			So(x, ShouldEqual, 2)
+		})
+	})
+
+	ethSignature := "0x61d864a4e35f81c3907a1485a638ff6aab3d4dc9c3ebee77c68ef1e89597c9663a45056f79851453cd1fbcf6c664161170d67ad06fe5f9da3266afcebeec40ce1c"
+	ebase := EcRevoerFeilds{
+		EthAddress:        "0x627306090abab3a6e1400e9345bc60c78a8bef57",
+		Msg:               "idhub",
+		EthereumSignature: ethSignature,
+	}
+
+	Convey("Test EC Recover", t, func() {
+		contain, err := Resolve(ebase)
+		So(err, ShouldEqual, nil)
+		So(*contain, ShouldEqual, ebase.EthAddress)
+	})
+}
